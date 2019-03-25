15c837cd5221a15b39ab5447f1d986b37fd908aa
Carlo <CRefice@gmail.com>
2019-03-25 12:24:56 +0100

    Launch devenv in subshell

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 function devenv() {
-if [[ $# -eq 0 ]]; then
-	DIR="$(pwd)"
-else
-	DIR="$1"
-fi
-cd $DIR
-SESS_NAME="$(basename $DIR)"
-tmux new-session -d -s "$SESS_NAME"
-tmux rename-window "code"
-tmux split-window -h
-tmux resize-pane -R 25
-tmux -2 attach-session -d
+	if [[ $# -eq 0 ]]; then
+		DIR="$(pwd)"
+	else
+		DIR="$1"
+	fi
+	SESS_NAME="$(basename $DIR)"
+	(cd $DIR; launch_tmux)
+}
+
+function launch_tmux() {
+	tmux new-session -s "$SESS_NAME" -d
+	tmux rename-window "code"
+	tmux split-window -h
+	tmux resize-pane -R 25
+	tmux -2 attach-session -d
 }
