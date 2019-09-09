d8f2ea11c39edd57877c122ca16db9209b214099
Carlo <CRefice@gmail.com>
2019-09-09 09:49:13 +0200

    Add eyesaver script

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -6,13 +6,13 @@ function devenv() {
 		DIR="$1"
 	fi
 	SESS_NAME="$(basename $DIR)"
-	(cd $DIR; launch_tmux)
+	(cd $DIR && launch_tmux)
 }
 
 function launch_tmux() {
 	tmux new-session -s "$SESS_NAME" -d
 	tmux rename-window "code"
 	tmux split-window -h
-	tmux resize-pane -R 25
+	tmux resize-pane -R 22
 	tmux -2 attach-session -d
 }
---
+++ scripts/eyesaver.sh
@@ -0,0 +1,4 @@
+while true; do
+	sleep 20m
+	notify-send 'Break time!' 'You'\''ve been looking at your screen for 20 minutes!' -i ~/scripts/eye.png
+done
