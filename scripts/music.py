5caed7b76cb652073270a54d7a3c378a25f0d784
Carlo <CRefice@gmail.com>
2019-12-28 14:21:26 +0100

    Switch to spaceship prompt, update zshrc

--- scripts/music.py
+++ scripts/music.py
@@ -2,10 +2,12 @@
 
 import os
 import sys
-import gi
-gi.require_version('Playerctl', '1.0')
-from gi.repository import Playerctl, GLib
 from subprocess import Popen
+
+import gi
+from gi.repository import GLib, Playerctl
+
+gi.require_version("Playerctl", "1.0")
 
 pid = str(os.getpid())
 pidfile = "/tmp/musicd.pid"
@@ -14,15 +16,17 @@ if os.path.isfile(pidfile):
     print("{} already exists, exiting".format(pidfile))
     sys.exit()
 
-with open(pidfile, 'w') as f:
+with open(pidfile, "w") as f:
     f.write(pid)
 
+
 def on_track_change(player, e):
-    Popen(['polybar-msg', 'hook', 'music', '1'])
+    Popen(["polybar-msg", "hook", "music", "1"])
+
 
 try:
     player = Playerctl.Player()
-    player.on('metadata', on_track_change)
+    player.on("metadata", on_track_change)
     GLib.MainLoop().run()
 finally:
     os.unlink(pidfile)
