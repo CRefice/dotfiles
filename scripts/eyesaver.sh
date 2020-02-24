f7a15bb5f13662c39ca284f749e2fa51fc4913d3
Carlo <CRefice@gmail.com>
2020-02-24 15:43:09 +0100

    Set eyesaver notifications to expire after 30sec, remove annoying dunst click indicator

--- scripts/eyesaver.sh
+++ scripts/eyesaver.sh
@@ -1,4 +1,1 @@
-while true; do
-	sleep 20m
-	notify-send 'Break time!' 'You'\''ve been looking at your screen for 20 minutes!' -i ~/scripts/eye.png
-done
+notify-send 'Break time!' 'You'\''ve been looking at your screen for 30 minutes!' -i ~/scripts/eye.png -t 30000
